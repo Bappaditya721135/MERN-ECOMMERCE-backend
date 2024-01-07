bf41d2f5917cdf18e58a18c9bd2832f946c94104
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2024-01-07 08:53:38 +0530

    add send eamil function for password reset

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,8 +5,21 @@ export const sendEamil = async (emailOp) => {
     try {
         const transporter = nodemailer.createTransport({
             service: process.env.SMTP_SERVICE,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD
+            }
         })
+
+        const mailOption = {
+            from: process.env.SMTP_USER,
+            to: emailOp.email,
+            subject: emailOp.subject,
+            text: emailOp.message
+        }
+
+        await transporter.sendMail(mailOption);
     } catch (error) {
-        
+        return error;
     }
 }
