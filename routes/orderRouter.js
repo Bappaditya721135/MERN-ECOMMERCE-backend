4d0e561d7879e5b161f7c645be4dff21bc64a190
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2024-01-13 10:14:20 +0530

    add routes for getOrderDetails and myOrder

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,15 +1,23 @@
 import express from "express";
 // CONTROLLERS 
-import { createNewOrder } from "../controller/orderController.js";
+import { createNewOrder, getOrderDetails, myOrder } from "../controller/orderController.js";
 // MIDDLEWARRE 
 import { isAuthenticated } from "../middleware/isAuthenticated.js"
+import { isAuthorize } from "../middleware/isAuthorize.js";
 
 
 // ORDER ROUTER  
 const orderRouter = express.Router();
 
-
+// CREATE NEW ORDER 
 orderRouter.route("/order/new").post(isAuthenticated, createNewOrder);
+
+// GET ORDER DETAILS FOR --Admin 
+orderRouter.route("/order/:orderId").get(isAuthenticated, isAuthorize("admin"), getOrderDetails);
+
+// GET MY ORDER 
+orderRouter.route("/orders/me").get(isAuthenticated, myOrder);
+
 
 
 
