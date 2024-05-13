e85a5de2573a369d0058071badaa49b0281475ae
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2024-05-13 18:47:41 +0530

    fix isAuthenticated

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -4,12 +4,13 @@ import jwt from "jsonwebtoken";
 
 export const isAuthenticated = async (req, res, next) => {
     try {
-       const { token } = req.cookies;
-       if(!token) {
+       const { authToken } = req.cookies;
+       console.log(authToken)
+       if(!authToken) {
         return next(new ErrorHandler("please login first", 403));
        }
     //    req.user = await
-    const decodeToken = jwt.verify(token, process.env.JWT_SECRECT_KEY);
+    const decodeToken = jwt.verify(authToken, process.env.JWT_SECRECT_KEY);
     req.user = await UserModel.findById(decodeToken.id);
        next();
     } catch (error) {
