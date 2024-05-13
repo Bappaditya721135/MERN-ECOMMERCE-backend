c181a3af8a088f92a927f6f115ad60a76f6cb586
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2024-05-13 18:48:49 +0530

    change token name from token to authToken

--- utils/sendCookie.js
+++ utils/sendCookie.js
@@ -1,7 +1,8 @@
 import jwt from "jsonwebtoken";
+import { UserModel } from "../models/userModel.js";
 
 
-export const sendCookie = (user, statusCode, res) => {
+export const sendCookie = async (user, statusCode, res) => {
     const cookieValidity = process.env.COOKIE_VALIDITY * 24 * 60 * 60 * 1000;
     const cookieOption = {
         httpOnly: true,
@@ -10,10 +11,30 @@ export const sendCookie = (user, statusCode, res) => {
 
     }
 
-    const token = user.getJWTToken();
-    res.status(statusCode).cookie("token", token, cookieOption).json({
+    const authToken = await user.getJWTToken(cookieValidity);
+    res.status(statusCode).cookie("authToken", authToken, cookieOption).json({
         success: true,
         user,
-        token
+        authToken
     })
+}
+
+
+
+// SEND EXISTING COOKIE 
+export const sendExistingCookie =  (user, statusCode, res) => {
+    console.log("existing cookie")
+    const cookieOption = {
+        httpOnly: true,
+        maxAge: user.authTokenExpiresInMiliSeconds,
+        sameSite: true
+    }
+
+    const authToken = user.authToken;
+    res.status(statusCode).cookie("authToken", authToken, cookieOption).json({
+        success: true,
+        user,
+        authToken
+    })
+
 }
